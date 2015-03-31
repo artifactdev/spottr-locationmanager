1e67137decb125998a221307ea2dbc452d1369dd
Jeremias Arnstadt <Jeremias.Arnstadt@t-systems.com>
2015-03-31 09:49:03 +0200

    #6 added grid in adminsitration for a better view of items

--- assets/js/administration.js
+++ assets/js/administration.js
@@ -27,21 +27,18 @@ function fillVerwaltung(json) {
             '<li>' +
             '<div class="item" id="' + json.data[i].id + '">' +
                 '<a href="#" class="image">' +
-                    '<div class="inner">' +
-                        '<div class="item-specific">' +
-                        '</div>' +
-                        '<img src="' + json.data[i].gallery + '" alt="">' +
-                    '</div>' +
+                    '<img src="' + json.data[i].gallery + '" alt="">' +
                 '</a>' +
                 '<div class="wrapper">' +
                     '<a href="#" class="quick-preview" id="' + json.data[i].id + '" data-gallery="' + json.data[i].gallery + '" data-title="' + json.data[i].title +'" data-type="' + json.data[i].type +'"  data-category="' + json.data[i].category +'" data-location="' + json.data[i].location +'" data-aperture="' + json.data[i].aperture +'" data-date="' + json.data[i].date +'" data-focal="' + json.data[i].focal +'" data-iso="' + json.data[i].iso +'" data-rating="' + json.data[i].rating +'"><h3>' + json.data[i].title + '</h3></a>' +
                     '<figure>' + json.data[i].category + '</figure>' +
-                    '<div class="info">' +
-                        '<div class="type">' +
-                            '<i><img src="' + json.data[i].type_icon + '" alt=""></i>' +
-                            '<span>' + json.data[i].type + '</span>' +
-                        '</div>' +
-                        '<div class="rating" data-rating="' + json.data[i].rating + '"></div>' +
+                '</div>' +
+                '<div class="col-md-12 item-footer">' +
+                    '<div class="col-md-6">' +
+                        '<a href="#" class="btn btn-default">Edit</a>' +
+                    '</div>' +
+                    '<div class="col-md-6">' +
+                        '<a href="#" class="btn btn-red">Delete</a>' +
                     '</div>' +
                 '</div>' +
             '</div>' +
