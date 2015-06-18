529cf8347f2a3fa4c5081876ce5857523258f2a9
Markus Jahn <markus.jahn@free2work.de>
2015-06-18 21:05:21 +0200

    Bugfixing

--- rest-api/docu/mysql_install.sql
+++ rest-api/docu/mysql_install.sql
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `locations` (
   `focal` int(1) NOT NULL,
   `iso` int(7) NOT NULL,
   `note` text NOT NULL,
-  `type` varchar(255) NOT NULL
+  `type` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
