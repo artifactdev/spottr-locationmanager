1f64efeb001fab7e4fd97eb13e31498c81fb9768
Jeremias Arnstadt <j.arnstadt@art-ifact.de>
2015-08-01 15:35:24 +0200

    Testcommit

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en-US" style="height: auto;">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <meta charset="UTF-8">
+        <meta charset="UTF-8">   
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="./assets/styles/bootstrap.css" type="text/css">
         <link rel="stylesheet" href="./assets/styles/bootstrap-select.min.css" type="text/css">
