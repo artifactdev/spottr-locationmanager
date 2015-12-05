54895333e48b03564916df0bc071c4cacd39f3e5
Jeremias Arnstadt <j.arnstadt@art-ifact.de>
2015-12-05 18:04:49 +0100

    #2 styled loginpage

--- login.php
+++ login.php
@@ -11,64 +11,49 @@
         <title>Spottr</title>
     </head>
     <body onunload="" class="page-login navigation-off-canvas page-fade-in" id="page-top">
-        <!-- Outer Wrapper-->
-        <div id="outer-wrapper">
-            <!-- Inner Wrapper -->
-            <div id="inner-wrapper">
-                <!-- Navigation-->
-                <?php
-                    include 'assets/moduls/header.php';
-                ?>
-                <!-- end Navigation-->
-                <!-- Page Canvas-->
-                <div id="page-canvas">
-                    <!--Page Content-->
-                    <div id="page-content">
-                        <div class="login-box">
-                            <form action="authentication">
-                                <input type="text" id="email" name="email" placeholder="Username" />
-                                <input type="password" id="password" name="password" placeholder="Password" />
-                                <button type="submit" class="btn btn-default">Login</button>
-                            </form>
-                        </div>
-                    </div>
-                    <!-- end Page Canvas-->
-                    <!--Page Footer-->
-                    <?php
-                        include 'assets/moduls/footer.php';
-                    ?>
-                    <!--end Page Footer-->
+        <?php
+            include 'assets/moduls/header.php';
+        ?>
+        <main>
+            <div class="row">
+                <div class="login-box">
+                    <form action="authentication">
+                        <input type="text" id="email" name="email" placeholder="Username" />
+                        <input type="password" id="password" name="password" placeholder="Password" />
+                        <button type="submit" class="btn btn-default pull-right">Login</button>
+                    </form>
                 </div>
-                <!-- end Inner Wrapper -->
+
             </div>
-            <!-- end Outer Wrapper-->
+        </main>
 
-             <!-- start libs -->
-            <script type="text/javascript" src="https://maps.google.com/maps/api/js?sensor=true&amp;libraries=places"></script>
-            <script type="text/javascript" src="./assets/js/libs/jquery-2.1.0.min.js"></script>
-            <script type="text/javascript" src="./assets/js/libs/bootstrap.min.js"></script>
-            <script type="text/javascript" src="./assets/js/libs/materialize.min.js"></script>
+        <?php
+            include 'assets/moduls/footer.php';
+        ?>
 
-            <!-- end libs -->
+         <!-- start libs -->
+        <script type="text/javascript" src="https://maps.google.com/maps/api/js?sensor=true&amp;libraries=places"></script>
+        <script type="text/javascript" src="./assets/js/libs/jquery-2.1.0.min.js"></script>
+        <script type="text/javascript" src="./assets/js/libs/bootstrap.min.js"></script>
+        <script type="text/javascript" src="./assets/js/libs/materialize.min.js"></script>
 
-            <!-- start plugins -->
-            <script type="text/javascript" src="./assets/js/plugins/gmaps.js"></script>
-            <script type="text/javascript" src="./assets/js/plugins/jqueryExtensions.js"></script>
-            <script type="text/javascript" src="./assets/js/plugins/jquery.cookie.js"></script>
-            <script type="text/javascript" src="./assets/js/plugins/jquery.validate.js"></script>
-            <script type="text/javascript" src="./assets/js/plugins/jquery.validate.bootstrap.js"></script>
-            <script type="text/javascript" src="./assets/js/plugins/spin.min.js"></script>
-            <script type="text/javascript" src="./assets/js/plugins/iosOverlay.js"></script>
+        <!-- end libs -->
 
-            <!-- end plugins -->
+        <!-- start plugins -->
+        <script type="text/javascript" src="./assets/js/plugins/gmaps.js"></script>
+        <script type="text/javascript" src="./assets/js/plugins/jqueryExtensions.js"></script>
+        <script type="text/javascript" src="./assets/js/plugins/jquery.cookie.js"></script>
+        <script type="text/javascript" src="./assets/js/plugins/jquery.validate.js"></script>
+        <script type="text/javascript" src="./assets/js/plugins/jquery.validate.bootstrap.js"></script>
+        <script type="text/javascript" src="./assets/js/plugins/spin.min.js"></script>
+        <script type="text/javascript" src="./assets/js/plugins/iosOverlay.js"></script>
 
-            <!-- start application -->
-            <script type="text/javascript" src="./assets/js/global.js"></script>
-            <script type="text/javascript" src="./assets/js/ajaxHandler.js"></script>
-            <script type="text/javascript" src="./assets/js/authenticationHelper.js"></script>
-            <script type="text/javascript" src="./assets/js/login.js"></script>
+        <!-- end plugins -->
 
-            <!-- end application -->
-        </div>
+        <!-- start application -->
+        <script type="text/javascript" src="./assets/js/global.js"></script>
+        <script type="text/javascript" src="./assets/js/ajaxHandler.js"></script>
+        <script type="text/javascript" src="./assets/js/authenticationHelper.js"></script>
+        <script type="text/javascript" src="./assets/js/login.js"></script>
     </body>
 </html>
